98f01e256d7f6c17c4a4cd35f134d0737236973d
ranbirverma <ranbirverma.25@techsmaters.com>
2025-07-24 17:47:05 +0530

    added navbar in app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,34 @@
-"use client"
+'use client';
 import { useThemeStore } from '@/store/useThemeStore';
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 
 const Footer = () => {
-    const { theme } = useThemeStore();
+  const { theme } = useThemeStore();
   return (
-    <footer className={` ${theme =="dark" ? "bg-gray-800 text-white border-t-2 " : "bg-white text-gray-900"} p-4 sm:p-6 flex flex-col items-center gap-4 sm:gap-6`}>
-      <Image src="/logo.png" alt="logo" width={100} height={100} className="w-20 h-20 sm:w-24 sm:h-24" />
+    <footer
+      className={` ${theme == 'dark' ? 'bg-gray-800 text-white border-t-2 ' : 'bg-white text-gray-900'} p-4 sm:p-6 flex flex-col items-center gap-4 sm:gap-6`}
+    >
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width={100}
+        height={100}
+        className="w-20 h-20 sm:w-24 sm:h-24"
+      />
       <p className="text-center text-sm sm:text-base">
         Copyright &copy; {new Date().getFullYear()} by{' '}
-        <a href="https://github.com/#" target="_blank" rel="noopener noreferrer" className="hover:underline">
-            Your Name
+        <a
+          href="https://github.com/#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          Your Name
         </a>
       </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
